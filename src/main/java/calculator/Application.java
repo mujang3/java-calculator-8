55557af3ab92f3ee7509ee00145d74433cf9a0bd
carpbreadj <carpbreadj@gmail.com>
2025-10-19 12:42:29 +0900

    feat:read input via Console API

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해주세요.");
 
+        String input = Console.readLine();
         System.out.println("결과 : ");
     }
 }
