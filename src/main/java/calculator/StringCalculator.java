4d23a3ddb2309d92b6e51d306309368c8d69d465
carpbreadj <carpbreadj@gmail.com>
2025-10-19 12:56:58 +0900

    feat:Implement default delimiters(comma, colon):

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,31 @@
 package calculator;
 
+import java.util.*;
+
 public class StringCalculator {
+
+    private static final String DELIMITER = "[,:]";
+
     public int add(String input) {
         if (input == null || input.isBlank()){
             return 0;
         }
-        return 0;
+        String experssion = input.trim();
+        String delimiter = DELIMITER;
+
+        String[] tokens = experssion.split(delimiter, -1);
+        int sum = 0;
+        for (String raw : tokens) {
+            String s = raw.trim();
+            if (s.isEmpty() || !s.matches("\\d+")){
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
+            int value = Integer.parseInt(s);
+            if (value < 0){
+                throw new IllegalArgumentException("잘못된 입력입니다/");
+            }
+            sum += value;
+        }
+        return sum;
     }
 }
